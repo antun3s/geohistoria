5156a3e37df4e8014fb2398a4be50e9c349f01f2
Bruno Antunes <antunes.dll@gmail.com>
2026-07-24 19:37:16 -0300

    Troca tile layer para mapa sem rótulos políticos

--- js/map.js
+++ js/map.js
@@ -45,8 +45,9 @@ const GameMap = (function () {
             attributionControl: true
         });
 
-        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            attribution: "&copy; OpenStreetMap contributors",
+        L.tileLayer("https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png", {
+            attribution: "&copy; OpenStreetMap contributors, &copy; CARTO",
+            subdomains: "abcd",
             maxZoom: 18
         }).addTo(map);
 
